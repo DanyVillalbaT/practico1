fbd26105657d7bcb4dadf599c1a54c31a751feee
Dany Villalba <villalba.daniela.0874@eam.edu.co>
2023-04-20 13:20:25 -0500

    Creada la clase producto con atributos

---
+++ src/practico1/Producto.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package practico1;
+
+/**
+ *
+ * @author daniv
+ */
+public class Producto {
+    
+    private int id;
+    private String name;
+    private double price;
+    
+}
